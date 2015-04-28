0315558b79de8f579789f13f29549c5d1f2b90fc
nobody1986 <as_star@qq.com>
2015-04-29 00:28:45 +0800

    yaml

---
+++ lib/Yaml.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace lib;
+
+/**
+ * Description of Yaml
+ *
+ * @author snow
+ */
+class Yaml {
+    static function parse($yaml){
+        $len = strlen($yaml);
+        for($i=0;$i<$len;++$i){
+            
+        }
+    }
+}
